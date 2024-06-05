7cb5b5c9ca0b8f8a0846f4faed898debf7e34b74
Benjamin Shen <bshen7225@gmail.com>
2024-06-05 02:25:55 -0400

    Controller implementation.

--- src/controller/StockController.java
+++ src/controller/StockController.java
@@ -1,7 +1,13 @@
 package controller;
 
+
+import model.user.UserData;
+
 /**
  * Class for the user to control the program.
  */
 public class StockController {
+  private Readable readable;
+  private Appendable appendable;
+  private UserData userData;
 }
