a489175c7d58984a6113a35a63a32c8bd94d3bf6
Benjamin Shen <bshen7225@gmail.com>
2024-06-05 02:29:03 -0400

    .

--- src/controller/StockController.java
+++ src/controller/StockController.java
@@ -7,11 +7,7 @@ import model.user.UserData;
  * Class for the user to control the program.
  */
 public class StockController {
-<<<<<<< HEAD
   private Readable readable;
   private Appendable appendable;
   private UserData userData;
-=======
-
->>>>>>> a93e281c9ec34ef33f16b4fd727798fa48fe738f
 }
